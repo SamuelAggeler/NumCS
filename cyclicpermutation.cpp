e895d51aa4fcae8e7029dc305f2c2953671afb65
Samuel Aggeler <saaggeler@student.ethz.ch>
2021-10-10 16:05:41 +0200

    added more exercises deleted unnecessary file

---
+++ cyclicpermutation.cpp
@@ -0,0 +1,15 @@
+#include "eigen/Eigen/Dense"
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Test validity of cyclic permutation implementation in Eigen"
+              <<std::endl;
+    Eigen::VectorXd v{Eigen::VectorXd::LinSpaced(10, 1, 10)};
+    std::cout << "Column vector v = " << v.transpose() << std::endl;
+
+    const int n = v.size();
+    v << v[n-1], v.head(n-1);
+    std::cout << "Permuted column vector v = " << v.transpose() << std::endl;
+
+    return 0;
+}
