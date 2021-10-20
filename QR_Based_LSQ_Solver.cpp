e50c07d0158eb56e9023560258ceb38de26fd495
Samuel Aggeler <samuel.aggeler@outlook.com>
2021-10-20 23:11:58 +0200

    added runtime

--- QR_Based_LSQ_Solver.cpp
+++ QR_Based_LSQ_Solver.cpp
@@ -1,6 +1,7 @@
 #include "eigen/Eigen/Dense"
 
 double lsqsolve_eigen(const Eigen::MatrixXd& A, const Eigen::VectorXd& b, Eigen::VectorXd& x) {
+    //Cost of =(n^2*m)
     x = A.householderQr().solve(b);
     return ((A*x-b).norm());
 }
